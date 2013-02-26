9af02169e5fd2b526d888fa05feb9d7b6e64d9c8
arahim@tssg.org <arahim@tssg.org>
2013-02-26 12:09:57 +0000

    update file /cloud/main.js

--- cloud/main.js
+++ cloud/main.js
@@ -8,16 +8,12 @@ exports.myFunction = function(params, callback) {
 
 exports.myFunction2 = function(params, callback) {
   var num = params.num;
-  if(num > 10){
-    return callback({err: 'Num too large'}, {result: num});
-  }
   callback(undefined, {result: num});
 };
 
 exports.myFunction3 = function(params, callback) {
   var num = params.num;
-  if(num > 10){
+  
     return callback({err: 'Num too large'}, {result: num});
-  }
   callback(undefined, {result: num});
 };
